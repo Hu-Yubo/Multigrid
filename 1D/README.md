da79073afc8360891e297804ddf2069bdf5f48ab
Hu-Yubo <21935029@zju.edu.cn>
2020-10-20 00:59:40 +0800

    Multigrid Solver for 1D possion equation

---
+++ 1D/README.md
@@ -0,0 +1,2 @@
+# Multigrid
+Slove 1D possion equation by multigrid method
---
+++ 1D/RestrictionOperator.h
@@ -0,0 +1,43 @@
+/**
+ * @file   RestrictionOperator.h
+ * @author HirasawaYui <yui@Ubuntu18-04>
+ * @date   Wed Oct 14 12:30:33 2020
+ * 
+ * @brief  2 kinds of restriction operator for MultigridSolver
+ * 
+ * 
+ */
+
+#include <iostream>
+#include <vector>
+#include <math.h>
+
+class RestrictionOperator
+{
+protected:
+    std::vector<double> _Input;
+    std::vector<double> _Output;
+public:
+    RestrictionOperator();
+    RestrictionOperator(std::vector<double> a);
+    void PrintInput();
+    void PrintOutput();
+    void SetInput(std::vector<double> a);
+    std::vector<double> ReturnOutput();
+    virtual void PrintType();
+    virtual void restrict();
+};
+
+class InjectionRestriction : public RestrictionOperator
+{
+public:
+    void restrict();
+    void PrintType();
+};
+
+class FullWeightingRestriction : public RestrictionOperator
+{
+public:
+    void restrict();
+    void PrintType();
+};
